12fc65d1fb4c9e79f23d4dc8c9e936341036e6de
alysondamiao <78306480+alysondamiao@users.noreply.github.com>
2023-06-08 14:51:48 +0000

    alterando arquivo pages.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-    return <h1>Dradri, veja só meu projeto inicial do curso!</h1>    
+    return <h1>Dradri, veja só meu projeto inicial do curso!!!</h1>    
 }
 export default Home;
