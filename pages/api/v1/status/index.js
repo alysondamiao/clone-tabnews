029224f94c21d5ebfaccfbafba402f3f4990265d
alysondamiao <78306480+alysondamiao@users.noreply.github.com>
2023-11-30 21:37:09 +0000

    add new properties to the status api

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ dados: "alunos do curso.dev são feras" });
+  const updatedAt = new Date().toISOString();
+
+  const query = `
+  SELECT version(),
+         setting AS max_connections
+  FROM pg_settings
+  WHERE name = 'max_connections';
+ 
+  SELECT COUNT(*) AS open_connections
+  FROM pg_stat_activity
+  WHERE state = 'active';
+
+`;
+
+  const queryResult = await database.query(query);
+  console.log(queryResult);
+  const dataBaseVersion = queryResult[0].rows[0].version;
+  const maxConnections = queryResult[0].rows[0].max_connections;
+  const openConnections = queryResult[1].rows[0].open_connections;
+  response.status(200).json({
+    updated_at: updatedAt,
+    data_base_version: dataBaseVersion,
+    max_connections: maxConnections,
+    open_connections: openConnections,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,11 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+  const parsedUpdatedat = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedat);
+  expect(responseBody.data_base_version).toBeDefined();
+  expect(responseBody.max_connections).toBeDefined();
+  expect(responseBody.open_connections).toBeDefined();
 });
