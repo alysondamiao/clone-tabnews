a977b4ea9e6451e13dc46c4674232d45262b6b78
alysondamiao <78306480+alysondamiao@users.noreply.github.com>
2023-12-30 14:17:56 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,28 +3,32 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const query = `
-  SELECT version(),
-         setting AS max_connections
-  FROM pg_settings
-  WHERE name = 'max_connections';
- 
-  SELECT COUNT(*) AS open_connections
-  FROM pg_stat_activity
-  WHERE state = 'active';
+  const dataBaseVersionResult = await database.query(`SHOW server_version;`);
+  const dataBaseVersionValue = dataBaseVersionResult.rows[0].server_version;
 
-`;
+  const dataBaseMaxConnectionResult = await database.query(
+    `SHOW max_connections;`
+  );
+  const maxConnectionsValue =
+    dataBaseMaxConnectionResult.rows[0].max_connections;
 
-  const queryResult = await database.query(query);
-  console.log(queryResult);
-  const dataBaseVersion = queryResult[0].rows[0].version;
-  const maxConnections = queryResult[0].rows[0].max_connections;
-  const openConnections = queryResult[1].rows[0].open_connections;
+  const dataBaseName = process.env.POSTGRES_DB;
+  const dataBaseOpenConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int AS open_connections FROM pg_stat_activity WHERE datname = $1",
+    values: [dataBaseName],
+  });
+  const openConnectionsValue =
+    dataBaseOpenConnectionsResult.rows[0].open_connections;
+
   response.status(200).json({
     updated_at: updatedAt,
-    data_base_version: dataBaseVersion,
-    max_connections: maxConnections,
-    open_connections: openConnections,
+    dependencies: {
+      database: {
+        version: dataBaseVersionValue,
+        max_connections: parseInt(maxConnectionsValue),
+        open_connections: openConnectionsValue,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,10 +2,11 @@ test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
   const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
+
   const parsedUpdatedat = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedat);
-  expect(responseBody.data_base_version).toBeDefined();
-  expect(responseBody.max_connections).toBeDefined();
-  expect(responseBody.open_connections).toBeDefined();
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.open_connections).toEqual(1);
 });
