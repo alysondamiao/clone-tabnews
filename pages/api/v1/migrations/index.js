397fd519447cfedc68cfc4de2f4dcdd87c66bc38
Alyson Trindade <alysondamiao@gmail.com>
2025-01-04 09:43:14 -0300

    some improvements on close connections

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,17 +2,24 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 import database from "infra/database.js";
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`,
+    });
+  }
+  let dbClient;
   try {
+    dbClient = await database.getNewClient();
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+
     if (request.method === "GET") {
       const pendingMigrations = await migrationRunner(defaultMigrationOptions);
       return response.status(200).json(pendingMigrations);
@@ -28,7 +35,6 @@ export default async function migrations(request, response) {
 
       return response.status(200).json(migratedMigrations);
     }
-    return response.status(405).end();
   } catch (error) {
     console.log(error);
     throw error;
